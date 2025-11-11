7ce8bb46ae575aa3a11e449b93cad9f841022067
Beatriz Alves <112479242+beacastroalves@users.noreply.github.com>
2025-11-11 18:17:05 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1 style={{margin: "16px"}}>Os circuitos de consagração social serão tanto mais eficazes quanto maior a distância social do objeto consagrado</h1>
+}
+
+export default Home;
