f81c2d7eca04143f3899964b1e6f9c81013b23b9
Beatriz Alves <112479242+beacastroalves@users.noreply.github.com>
2025-11-13 20:20:43 +0000

    altera a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1 style={{margin: "16px"}}>Melhor usar pantufas do que tentar colocar carpete no mundo inteiro.</h1>
+    return 
+        <h1 style={{display: "flex", justifyContent:"center", alignItems:"center", margin: "16px"}}>
+            O amor não é só olhar um para o outro, é também olhar juntos na mesma direção.
+        </h1>
 }
 
 export default Home;
