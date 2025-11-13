09f49ceb55feb9e17e9083918a7e49f1bdd2a15f
Beatriz Alves <112479242+beacastroalves@users.noreply.github.com>
2025-11-13 20:45:26 +0000

    Corrigindo erros no estilo e deixando mensagem mais completa

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
     return (
-        <div style="display: flex; justify-content: center;">
-            <h1 style="margin: 16px;">
+        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems:"center", margin:"16px"}}>
+            <h1 style={{ maxWidth:"380px",margin: "auto", marginTop: "64px", textAlign:"center" }}>
                 O amor não é só olhar um para o outro, é também olhar juntos na mesma direção.
             </h1>
+            <h2 style={{ maxWidth:"100%", marginTop: "24px", textAlign:"center" }}>Feliz <small>(quase)</small> 2 anos e 2 meses.</h2>
         </div>
     )
 }
