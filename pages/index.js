c9e6c1fe107e71eb45d8882ad5f05cb6d2cd0b1e
Beatriz Alves <112479242+beacastroalves@users.noreply.github.com>
2025-11-13 20:34:36 +0000

    altera o estilo da mensagem com div

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 function Home() {
-    return 
+    return (
         <div style="display: flex; justify-content: center;">
             <h1 style="margin: 16px;">
                 O amor não é só olhar um para o outro, é também olhar juntos na mesma direção.
             </h1>
         </div>
+    )
 }
 
 export default Home;
